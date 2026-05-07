68a4106ce672bc769c587bb2f867a9e47649d544
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-07 10:31:59 +0200

    set method

--- index.php
+++ index.php
@@ -10,9 +10,15 @@ class Movie {
     public function __construct($_name, $_main_actor, $_director, $_release_date) {
 
         $this->name = $_name;
-        $this->name = $_main_actor;
-        $this->name = $_director;
-        $this->name = $_release_date;
+        $this->main_actor = $_main_actor;
+        $this->director = $_director;
+        $this->release_date = $_release_date;
+
+    }
+
+    public function getInfo() {
+
+        return $this->name . "di" . $this->director . "con" . $this->main_actor . "uscirà nelle sale il giorno" . $this->release_date;
 
     }
 
