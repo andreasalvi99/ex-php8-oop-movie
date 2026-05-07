469883fa7f861c79552559630918eafe630a39e8
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-07 10:39:19 +0200

    print objects and methods

--- index.php
+++ index.php
@@ -2,10 +2,10 @@
 
 class Movie {
 
-    public name;
-    public main_actor;
-    public director;
-    public release_date;
+    public $name;
+    public $main_actor;
+    public $director;
+    public $release_date;
 
     public function __construct($_name, $_main_actor, $_director, $_release_date) {
 
@@ -18,13 +18,20 @@ class Movie {
 
     public function getInfo() {
 
-        return $this->name . "di" . $this->director . "con" . $this->main_actor . "uscirà nelle sale il giorno" . $this->release_date;
+        return $this->name . " di " . $this->director . " con " . $this->main_actor . " uscirà nelle sale il giorno " . $this->release_date;
 
     }
 
 }
 
+$movie1 = new Movie("Odissea", "Tom Holland", "Cristopher Nolan", "19.07.2026");
+$movie2 = new Movie("Spider-man: Brand New Day", "Tom Holland", "Destin Daniel Cretton", "20.!2.2026");
 
+var_dump($movie1);
+var_dump($movie2);
+
+// echo $movie1->getInfo();
+echo $movie2->getInfo();
 
 
 ?>
