2fc135f7db69da21f7d80828aef464a380fc0476
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-05-07 11:03:55 +0200

    set relationship between classes

--- index.php
+++ index.php
@@ -20,13 +20,15 @@ class Movie {
     public string $mainActor;
     public string $director;
     public string $releaseDate;
+    public $genre;
 
-    public function __construct($_name, $_mainActor, $_director, $_releaseDate) {
+    public function __construct($_name, $_mainActor, $_director, $_releaseDate, Genre $_genre) {
 
         $this->name = $_name;
         $this->mainActor = $_mainActor;
         $this->director = $_director;
         $this->releaseDate = $_releaseDate;
+        $this->genre = $_genre;
 
     }
 
@@ -38,8 +40,12 @@ class Movie {
 
 }
 
-$movie1 = new Movie("Odissea", "Tom Holland", "Cristopher Nolan", "19.07.2026");
-$movie2 = new Movie("Spider-man: Brand New Day", "Tom Holland", "Destin Daniel Cretton", "20.!2.2026");
+$movie1 = new Movie("Odissea", "Tom Holland", "Cristopher Nolan", "19.07.2026", new Genre("azione", "Il genere action comprende film ricchi di combattimenti, inseguimenti, esplosioni e scene ad alta tensione, con protagonisti spesso coinvolti in missioni pericolose o scontri contro nemici potenti.
+Praticamente persone che risolvono problemi complessi distruggendo mezza città. Metodo discutibile, ma molto cinematografico.
+", false));
+$movie2 = new Movie("Spider-man: Brand New Day", "Tom Holland", "Destin Daniel Cretton", "20.!2.2026", new Genre("fantascienza", "Il genere fantascienza comprende film ambientati in futuri tecnologici, universi spaziali o realtà alternative, esplorando temi come il progresso scientifico, l’intelligenza artificiale e il rapporto tra uomo e tecnologia.
+In pratica: “e se inventassimo qualcosa che poi ci distrugge?”. Tema sorprendentemente ricorrente nella storia umana.
+", false));
 
 var_dump($movie1);
 var_dump($movie2);
